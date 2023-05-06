08c7e1e40a8ac0270f078f1da6990062c7fd6c1b
Neel <neelnarayanshetty@gmail.com>
2023-05-06 10:57:15 +0530

    rn paper

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,41 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { SplashScreen, Stack } from 'expo-router';
-import { useEffect } from 'react';
-import { useColorScheme } from 'react-native';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+import { useColorScheme } from "react-native";
+import {
+  Provider as PaperProvider,
+  MD3LightTheme as PaperDefaultTheme,
+} from "react-native-paper";
+import { Provider } from "../context/auth";
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from 'expo-router';
+} from "expo-router";
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: "(tabs)",
+};
+
+const theme = {
+  ...PaperDefaultTheme,
+  colors: {
+    ...PaperDefaultTheme.colors,
+    primary: "tomato",
+    secondary: "yellow",
+  },
 };
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     ...FontAwesome.font,
   });
 
@@ -40,12 +58,18 @@ function RootLayoutNav() {
 
   return (
     <>
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-        </Stack>
-      </ThemeProvider>
+      <Provider>
+        <ThemeProvider
+          value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+        >
+          <PaperProvider>
+            <Stack>
+              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+            </Stack>
+          </PaperProvider>
+        </ThemeProvider>
+      </Provider>
     </>
   );
 }
---
+++ context/auth.tsx
@@ -0,0 +1,56 @@
+import { useRouter, useSegments } from "expo-router";
+import React from "react";
+
+type AuthContext = {
+  signIn: () => void;
+  signOut: () => void;
+} | null;
+
+const AuthContext = React.createContext<AuthContext>(null);
+// This hook can be used to access the user info.
+export function useAuth() {
+  return React.useContext(AuthContext);
+}
+
+// This hook will protect the route access based on user authentication.
+function useProtectedRoute(user: boolean) {
+  const segments = useSegments();
+  console.log(
+    "🚀 ~ file: auth.tsx:18 ~ useProtectedRoute ~ segments:",
+    segments
+  );
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const inAuthGroup = segments[0] === "(auth)";
+
+    if (
+      // If the user is not signed in and the initial segment is not anything in the auth group.
+      !user &&
+      !inAuthGroup
+    ) {
+      // Redirect to the sign-in page.
+      router.replace("/sign-in");
+    } else if (user && inAuthGroup) {
+      // Redirect away from the sign-in page.
+      router.replace("/");
+    }
+  }, [user, segments]);
+}
+
+export function Provider(props: { children: React.ReactNode }) {
+  const [user, setAuth] = React.useState(false);
+
+  useProtectedRoute(user);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        signIn: () => setAuth(false),
+        signOut: () => setAuth(true),
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+}
